92ea83dd7db5a9a2e73e613db26de3a312ba34a9
nikolaystanishev <stanishev.nikolay@gmail.com>
2021-11-24 02:00:18 +0200

    Format code

--- common/js/list.js
+++ common/js/list.js
@@ -9,7 +9,7 @@ var wid = 0;
 
 nav.find('ul li a').click((e) => {
   var _this = $(e.target);
-  if(!_this.parent().hasClass('active-list') && !nav.hasClass('animate')) {
+  if (!_this.parent().hasClass('active-list') && !nav.hasClass('animate')) {
     nav.addClass('animate');
 
     nav.find('ul li').removeClass('active-list');
@@ -17,8 +17,8 @@ nav.find('ul li a').click((e) => {
 
     var position = _this.position();
     var width = _this.parent().width();
-    
-    if(position.left >= pos) {
+
+    if (position.left >= pos) {
       line.animate({
         width: ((position.left - pos) + width)
       }, 300, () => {
@@ -58,13 +58,13 @@ $(window).bind("load", () => {
 function setupList() {
   var href = document.location.href.split('#')[1];
   nav.find('ul li').removeClass('active-list');
-  
+
   if (href) {
     nav.find(`a[href$="${href}"]`).addClass('active-list');
   } else {
-    $(nav.find('a')[0]).addClass('active-list');  
+    $(nav.find('a')[0]).addClass('active-list');
   }
-  
+
   var active = nav.find('a.active-list');
   active.click();
 }
